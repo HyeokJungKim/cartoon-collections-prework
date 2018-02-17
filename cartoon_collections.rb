7b5bd42430909e651510ac978cbc4b47d2738f94
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-17 01:27:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each do |word|
     if word.length > 4
-      return false
+      return true
     end
   end
   return true
