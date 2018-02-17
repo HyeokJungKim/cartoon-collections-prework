485c66c2384c1855f2168500ff816caf88ee9ef2
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-17 01:03:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index{|val,index|
+  puts "#{index} val"}
 end
 
 def summon_captain_planet# code an argument here
